fbc1ad082d0bff70c529862caf7a0978c60a15fc
Nfxxx <690749998@qq.com>
2024-09-27 23:06:47 +0800

    Initial commit

---
+++ binance/__init__.py
@@ -0,0 +1,12 @@
+"""An unofficial Python wrapper for the Binance exchange API v3
+
+.. moduleauthor:: Sam McHardy
+
+"""
+
+__version__ = "1.0.20"
+
+from binance.client import Client, AsyncClient  # noqa
+from binance.client_pm import  PortfolioClient
+from binance.depthcache import DepthCacheManager, OptionsDepthCacheManager, ThreadedDepthCacheManager  # noqa
+from binance.streams import BinanceSocketManager, ThreadedWebsocketManager  # noqa
